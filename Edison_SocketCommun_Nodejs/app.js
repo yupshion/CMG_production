75543760ca37124a1a0370ef0f9dd3b85b8521c3
mmkcoins <pgcello.33@gmail.com>
2016-01-19 19:38:52 +0900

    new controller files

--- Edison_SocketCommun_Nodejs/app.js
+++ Edison_SocketCommun_Nodejs/app.js
@@ -11,7 +11,7 @@ var http_port = 1337;
 
 app.use(express.static(__dirname + '/public'));
 app.get('/', function(req, res) {
-	res.sendfile(__dirname + '/public/index.html');
+	res.sendfile(__dirname + '/public/controller.html');
 });
 
 server.listen(http_port);
